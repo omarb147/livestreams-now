9bdf20be9a953ab39f162a1d3cf4686afdef7891
Omar Bello <omar.bello@clientrelationship.com>
2020-05-09 21:43:09 +0100

    updated database

--- scraper-function.js
+++ scraper-function.js
@@ -1,5 +1,9 @@
 const chromium = require("chrome-aws-lambda");
 const headers = require("./utils/headers");
+const AWS = require("aws-sdk");
+const uuid = require("uuid").v4;
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
   let browser = null;
@@ -12,7 +16,7 @@ exports.handler = async (event) => {
     headless: true,
     ignoreHTTPSErrors: true,
   });
-  
+
   const targetUrl = event.queryStringParameters.url;
   if (targetUrl.includes("ticketmaster.com")) {
     // TODO: function which takes the browser as an input to do whatever
@@ -21,6 +25,21 @@ exports.handler = async (event) => {
     // TODO : function which takes the browser as an input to do whatever
     result = "jambase";
   }
+  const params = {
+    TableName: process.env.LIVESTREAMS_TABLENAME,
+    Item: {
+      id: uuid(),
+      artist: "Drake",
+      date: new Date(),
+      imageUrl: "https://google.com",
+    },
+  };
+  console.log(params);
+  dynamodb.put(params, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 
   await browser.close();
   return {
