c195f5c09a1643a1f4a5ed09f161a931cfc17796
Omar Bello <omar.bello@clientrelationship.com>
2020-05-11 00:31:58 +0100

    got firebase working

--- scraper-function.js
+++ scraper-function.js
@@ -3,33 +3,79 @@ const headers = require("./utils/headers");
 const middy = require("/opt/middy-wrapper");
 const getJambaseData = require("./websites/scrapeJambase");
 
+var admin = require("firebase-admin");
+var serviceAccount = require("./config/config.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://gs-livestream.firebaseio.com",
+});
+
+const db = admin.firestore();
+
 const main = async (event) => {
-  let browser = null;
-  let result = null;
+  // let browser = null;
+  // let result = null;
 
-  browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
-  });
+  // browser = await chromium.puppeteer.launch({
+  //   args: chromium.args,
+  //   defaultViewport: chromium.defaultViewport,
+  //   executablePath: await chromium.executablePath,
+  //   headless: true,
+  //   ignoreHTTPSErrors: true,
+  // });
 
-  const targetUrl = event.queryStringParameters.url;
-  if (targetUrl.includes("ticketmaster.com")) {
-    // TODO: function which takes the browser as an input to do whatever
-    result = "ticketMaster";
-  } else if (targetUrl.includes("jambase.com")) {
-    // TODO : function which takes the browser as an input to do whatever
-    result = await getJambaseData(browser, targetUrl);
-  }
+  // const targetUrl = event.queryStringParameters.url;
+  // if (targetUrl.includes("ticketmaster.com")) {
+  //   // TODO: function which takes the browser as an input to do whatever
+  //   result = "ticketMaster";
+  // } else if (targetUrl.includes("jambase.com")) {
+  //   // TODO : function which takes the browser as an input to do whatever
+  //   result = await getJambaseData(browser, targetUrl);
+  // }
 
-  await browser.close();
+  // await browser.close();
+
+  const data = {
+    message: "Hello, world!",
+  };
+
+  await db
+    .collection("lambda-docs-test")
+    .add(data)
+    .then((ref) => {
+      // On a successful write, return an object
+      // containing the new doc id.
+    })
+    .catch((err) => {
+      // Forward errors if the write fails
+      console.log();
+      callback(err);
+    });
+
+  const test = await db
+    .collection("lambda-docs-test")
+    .get()
+    .then((snapshot) => {
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+      return data;
+    })
+    .catch((err) => {
+      // Forward errors if the write fails
+      console.log(err);
+      callback(err);
+    });
+
+  console.log(test);
+
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify(result),
+    body: JSON.stringify(test),
   };
 };
 
-exports.handler = middy(main);
+exports.handler = main;
