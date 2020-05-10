09da4d30ac9d477ea4473a0311dc9dba007c7529
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-10 13:56:55 +0100

    feat(jambase): added scraping results to jambase
    Co-authored-by: Omar <o_bello@live.co.uk>

--- scraper-function.js
+++ scraper-function.js
@@ -1,6 +1,7 @@
 const chromium = require("chrome-aws-lambda");
 const headers = require("./utils/headers");
 const middy = require("/opt/middy-wrapper");
+const getJambaseData = require("./websites/scrapeJambase");
 
 const main = async (event) => {
   let browser = null;
@@ -14,22 +15,20 @@ const main = async (event) => {
     ignoreHTTPSErrors: true,
   });
 
-  const targetUrl = event.queryStringParameters.url;
+  const targetUrl = "https://www.jambase.com/livestreams";
   if (targetUrl.includes("ticketmaster.com")) {
     // TODO: function which takes the browser as an input to do whatever
     result = "ticketMaster";
   } else if (targetUrl.includes("jambase.com")) {
     // TODO : function which takes the browser as an input to do whatever
-    result = "jambase";
+    result = await getJambaseData(browser, targetUrl);
   }
 
   await browser.close();
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify({
-      result,
-    }),
+    body: result,
   };
 };
 
