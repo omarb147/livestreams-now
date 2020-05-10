6afdc6c29fb1d2e7a56dd6b315ee7695ba57ca80
Omar Bello <omar.bello@clientrelationship.com>
2020-05-11 00:31:58 +0100

    got firebase working

---
+++ config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyCmB1_r19KSfNyoc4AxHBYmQByQr5P1kV8",
+  authDomain: "gs-livestream.firebaseapp.com",
+  databaseURL: "https://gs-livestream.firebaseio.com",
+  projectId: "gs-livestream",
+  storageBucket: "gs-livestream.appspot.com",
+  messagingSenderId: "566985182367",
+  appId: "1:566985182367:web:4e08d6f122fd280aec26aa",
+};
