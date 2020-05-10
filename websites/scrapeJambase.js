96ef5b98a175b5ae895175bf3a327f49b56a07ba
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-10 13:56:55 +0100

    feat(jambase): added scraping results to jambase
    Co-authored-by: Omar <o_bello@live.co.uk>

---
+++ websites/scrapeJambase.js
@@ -0,0 +1,34 @@
+module.exports = async (browser, targetURL) => {
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto(targetURL);
+  await page.waitForSelector("div.article-content");
+
+  const data = await page.evaluate(() => {
+    const callQuerySelector = (item, element, property) =>
+      item.querySelector(element) ? item.querySelector(element)[property] : "";
+    let results = [];
+    let items = document.querySelectorAll(
+      ".streams-list.js-stickybit-parent >ul >li >div.item-wrap >div.col-content"
+    );
+    try {
+      items.forEach((item) => {
+        let artist = callQuerySelector(item, "h3", "innerText");
+        let description = callQuerySelector(item, "p", "innerText");
+        let url = callQuerySelector(item, "a", "href");
+        let time = callQuerySelector(item, "ul li:first-child", "innerText");
+
+        results.push({
+          artsistName: artist,
+          descriptions: description,
+          source: url,
+          date: time,
+        });
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+    return results;
+  });
+  return data;
+};
