68d96a5de624cd275fa27c95f2205231d5b12760
Omar Bello <omar.bello@clientrelationship.com>
2020-05-10 10:16:05 +0100

    removed dynamodb test from scraper-function

--- scraper-function.js
+++ scraper-function.js
@@ -1,9 +1,5 @@
 const chromium = require("chrome-aws-lambda");
 const headers = require("./utils/headers");
-const AWS = require("aws-sdk");
-const uuid = require("uuid").v4;
-
-const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
   let browser = null;
@@ -25,21 +21,6 @@ exports.handler = async (event) => {
     // TODO : function which takes the browser as an input to do whatever
     result = "jambase";
   }
-  const params = {
-    TableName: process.env.LIVESTREAMS_TABLENAME,
-    Item: {
-      id: uuid(),
-      artist: "Drake",
-      date: new Date(),
-      imageUrl: "https://google.com",
-    },
-  };
-  console.log(params);
-  dynamodb.put(params, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-  });
 
   await browser.close();
   return {
