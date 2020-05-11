a441c73004c655bc16323dbc7fe8d1c73dc2b6ba
Omar Bello <omar.bello@clientrelationship.com>
2020-05-11 10:38:49 +0100

    refactored firebase to seperate file

--- scraper-function.js
+++ scraper-function.js
@@ -2,72 +2,32 @@ const chromium = require("chrome-aws-lambda");
 const headers = require("./utils/headers");
 const middy = require("/opt/middy-wrapper");
 const getJambaseData = require("./websites/scrapeJambase");
-
-var admin = require("firebase-admin");
-var serviceAccount = require("./config/config.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://gs-livestream.firebaseio.com",
-});
-
-const db = admin.firestore();
+const { getAllDataFromCollection } = require("./utils/firebase-libs");
 
 const main = async (event) => {
-  // let browser = null;
-  // let result = null;
+  let browser = null;
+  let result = null;
 
-  // browser = await chromium.puppeteer.launch({
-  //   args: chromium.args,
-  //   defaultViewport: chromium.defaultViewport,
-  //   executablePath: await chromium.executablePath,
-  //   headless: true,
-  //   ignoreHTTPSErrors: true,
-  // });
+  browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
 
-  // const targetUrl = event.queryStringParameters.url;
-  // if (targetUrl.includes("ticketmaster.com")) {
-  //   // TODO: function which takes the browser as an input to do whatever
-  //   result = "ticketMaster";
-  // } else if (targetUrl.includes("jambase.com")) {
-  //   // TODO : function which takes the browser as an input to do whatever
-  //   result = await getJambaseData(browser, targetUrl);
-  // }
+  const targetUrl = event.queryStringParameters.url;
+  if (targetUrl.includes("ticketmaster.com")) {
+    // TODO: function which takes the browser as an input to do whatever
+    result = "ticketMaster";
+  } else if (targetUrl.includes("jambase.com")) {
+    // TODO : function which takes the browser as an input to do whatever
+    result = await getJambaseData(browser, targetUrl);
+  }
 
-  // await browser.close();
+  await browser.close();
 
-  const data = {
-    message: "Hello, world!",
-  };
-
-  await db
-    .collection("lambda-docs-test")
-    .add(data)
-    .then((ref) => {
-      // On a successful write, return an object
-      // containing the new doc id.
-    })
-    .catch((err) => {
-      // Forward errors if the write fails
-      console.log();
-      callback(err);
-    });
-
-  const test = await db
-    .collection("lambda-docs-test")
-    .get()
-    .then((snapshot) => {
-      const data = [];
-      snapshot.forEach((doc) => {
-        data.push(doc.data());
-      });
-      return data;
-    })
-    .catch((err) => {
-      // Forward errors if the write fails
-      console.log(err);
-      callback(err);
-    });
+  const test = await getAllDataFromCollection("lambda-docs-test");
 
   console.log(test);
 
@@ -78,4 +38,4 @@ const main = async (event) => {
   };
 };
 
-exports.handler = main;
+exports.handler = middy(main);
--- utils/dynamo-libs.js
+++
@@ -1,8 +0,0 @@
-const AWS = require("aws-sdk");
-
-const client = AWS.DynamoDB.DocumentClient();
-
-module.exports = {
-  get: (params) => client.get(params).promise(),
-  put: (params) => client.put(params).promise(),
-};
