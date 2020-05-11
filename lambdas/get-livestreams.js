d2e2f7f18e4b96feedc2e636b9b7dcaacaa3596a
Brandonlawrence <lawrb123@googlemail.com>
2020-05-11 23:37:20 +0100

    Setup Lambda Function For Firebase Get Request Lambda Function is Getting Data From FB

--- lambdas/get-livestreams.js
+++ lambdas/get-livestreams.js
@@ -1,11 +1,13 @@
 const middy = require("/opt/middy-wrapper");
 const headers = require("../utils/headers");
+const firebaseRef = require('firebase/firebase-db');
 
 const main = async () => {
+    const dbData = await firebaseRef.getAllDataFromCollection('cities');
     return {
         statusCode: 200,
         headers,
-        body: JSON.stringify({message: 'Livestreams Successfully Received Retrieved'})
+        body: JSON.stringify(dbData)
     };
 }
 exports.handler = middy(main);
