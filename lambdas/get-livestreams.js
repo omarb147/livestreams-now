1d814f0077fba1a59eb4e72e45391dc90ed571f4
Omar Bello <omar.bello@clientrelationship.com>
2020-05-12 00:16:35 +0100

    updated for dynamic get fn

--- lambdas/get-livestreams.js
+++ lambdas/get-livestreams.js
@@ -1,13 +1,14 @@
 const middy = require("/opt/middy-wrapper");
 const headers = require("../utils/headers");
-const firebaseRef = require('firebase/firebase-db');
+const firebaseRef = require("../firebase/firebase-db");
 
-const main = async () => {
-    const dbData = await firebaseRef.getAllDataFromCollection('cities');
-    return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify(dbData)
-    };
-}
+const main = async (event) => {
+  const collection = event.queryStringParameters.collectionName;
+  const dbData = await firebaseRef.getAllDataFromCollection(collection);
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(dbData),
+  };
+};
 exports.handler = middy(main);
