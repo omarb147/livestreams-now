aa897ffcc8d6975a2289892eb84e1c590e514d71
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-11 20:18:47 +0100

    feat(lambda): add firbase validation

--- lambdas/scraper-function.js
+++ lambdas/scraper-function.js
@@ -4,14 +4,18 @@ const chromium = require("chrome-aws-lambda");
 const middy = require("/opt/middy-wrapper");
 // UTILITY FUNCTIONS
 const headers = require("../utils/headers");
-const { getAllDataFromCollection } = require("../firebase/firebase-db");
-// OTHER FUNCTIONS
+const {
+  getAllDataFromCollection,
+  addFilteredDocuments,
+} = require("../firebase/firebase-db");
+// PUPPETEER FUNCTIONS
 const getJambaseData = require("../websites/scrapeJambase");
 const getTicketMasterData = require("../websites/ticket-master");
 
 const main = async (event) => {
   let browser = null;
   let result = null;
+  let dbCollection = "";
 
   browser = await chromium.puppeteer.launch({
     args: chromium.args,
@@ -25,12 +29,16 @@ const main = async (event) => {
   if (targetUrl.includes("ticketmaster.co.uk")) {
     // TODO: function which takes the browser as an input to do whatever
     result = await getTicketMasterData(browser, targetUrl);
+    dbColection = "ticketmasterDB";
   } else if (targetUrl.includes("jambase.com")) {
     // TODO : function which takes the browser as an input to do whatever
     result = await getJambaseData(browser, targetUrl);
+    dbCollection = "jambaseDB";
   }
 
   await browser.close();
+
+  await addFilteredDocuments(result, dbCollection);
 
   return {
     statusCode: 200,
